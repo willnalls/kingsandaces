b2605974313c841aac72d196d139740e93d31548
willnalls <will.nalls@googlemail.com>
2019-07-23 12:40:57 -0400

    model checker:

--- example.pl
+++ example.pl
@@ -15,28 +15,49 @@ worlds(2).
 
 
 valuation(1,[p]).
-valuation(2.[q]).
+valuation(2,[q]).
 % world 1 satisfies p, world 2 satisfies q.
 
-a(1,1).
-a(2,2).
+relations(a,[[1],[2]]).
 % a has a minimal epistemic relation
 
-b(1,1).
-b(1,2).
-b(2,1).
-b(2,2).
+relations(b,[[1,2]]).
 % b has a maximal relation
 
 
 % END OF THE EXAMPLE EPISTEMIC MODEL
 
+% check whether X is a formula of the basic epistemic language
+
 is_formula(X) :- propositions(Y), member(X,Y).                    %propositions
-is_formula(X) :- X = [Y,Z], is_formula(Y), is_formula(Z).         %conjunction
-is_formula(X) :- X = ['n',Y], is_formula(Y).                      %negation
-is_formula(X) :- X = [Y,Z], agents(A), member(Y,A), is_formula(Z).%knowledge
+is_formula(X) :- X = [Y,Z], is_formula(Y), is_formula(Z).         %conjunction is of the form [Y,Z]
+is_formula(X) :- X = ['n',Y], is_formula(Y).                      %negation is of the form [n,Z]
+is_formula(X) :- X = [Y,Z], agents(A), member(Y,A), is_formula(Z).%possibility is of the form [i,Z], where i is in agents.
 
-satisfies(X,Y) :- worlds(Worlds), X < Worlds, is_formula(Y),		  
-		propositions(Z), member(Y,Z),
-		valuation(X,P), member(Y,P).
+% checks that X,Y is a world, formula pair
 
+world_formula(X,Y) :- worlds(Worlds), X =< Worlds, 1 =< X,  is_formula(Y).
+
+% reachability predicate: X can see Z from Y
+
+reachable(X,Y,Z) :- agents(A), member(X,A),  %X is an agent
+		    worlds(W), 1 =< Y, 1 =< Z, Y =< W, Z =< W, %Y, Z are worlds
+		    relations(X, P), member(P1,P), member(Y,P1), member(Z,P1).
+
+% satisfaction predicate definedon world, formula pairs
+
+satisfies(X,Y) :- world_formula(X,Y),      %X is a world, Y is a formula
+		propositions(Z), member(Y,Z),                     %Y is a proposition
+		valuation(X,P), member(Y,P).                      %Y is a member of X's valuation
+
+satisfies(X,Y) :- world_formula(X,Y),
+		  Y = [Z1,Z2], is_formula(Z1), is_formula(Z2),    %Y is a conjunction
+		  satisfies(X,Z1), satisfies(X,Z2).               %X satisfies both.     
+
+satisfies(X,Y) :- world_formula(X,Y),
+		  Y = ['n',Z], is_formula(Z), not(satisfies(X,Z)).    %Y is a negation, X does not satisfy Z
+
+
+satisfies(X,Y) :- world_formula(X,Y),
+		  Y = [A1,Z], agents(A), member(A1,A), is_formula(Z), %Y is an A1 possibility modality, where A1 is an agent
+		  relations(A1,P), member(P1,P), member(X,P1), member(X1,P1), satisfies(X1,Z).  
